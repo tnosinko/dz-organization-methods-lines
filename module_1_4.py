df0a8463a3b84ce46733785723ea7040c1f5243c
Татьяна <tnosinko@gmail.com>
2024-10-18 17:30:45 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+#my_string= input("Котик")
+#print(my_string [:4])
+#print (my_string,'Котик'.upper())
+#print (my_string,'Котик'.lower())
+my_string= "Очень красивый котик"
+print (my_string.replace(" ",""))
+print (my_string [0])
+print (my_string [-1])
