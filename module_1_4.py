05a2cfd0eee99085ac380e4c6a804450732fd5f5
Татьяна <tnosinko@gmail.com>
2024-10-31 10:16:22 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,8 +1,5 @@
-#my_string= input("Котик")
-#print(my_string [:4])
-#print (my_string,'Котик'.upper())
-#print (my_string,'Котик'.lower())
-my_string= "Очень красивый котик"
-print (my_string.replace(" ",""))
-print (my_string [0])
-print (my_string [-1])
+my_string= input()
+print ('количество символов в строке ',len(my_string))
+print (my_string.upper())
+print (my_string.lower())
+
