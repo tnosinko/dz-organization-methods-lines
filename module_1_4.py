9c5ee8e7208171b9ed21e1b84c8660ab13f906c5
Татьяна <tnosinko@gmail.com>
2024-11-07 18:08:31 +0300

    Update module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -2,4 +2,6 @@ my_string= input()
 print ('количество символов в строке ',len(my_string))
 print (my_string.upper())
 print (my_string.lower())
-
+print (my_string.replace(" ",""))
+print (my_string [0])
+print (my_string [-1])
